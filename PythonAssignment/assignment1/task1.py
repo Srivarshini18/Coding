c8480cb0a5b94913b210bb00390c20f802d6b752
Srivarshini Noothi <118071643+Srivarshini18@users.noreply.github.com>
2024-05-31 20:25:01 +0530

    Update and rename task1.txt to task1.py

---
+++ PythonAssignment/assignment1/task1.py
@@ -0,0 +1,16 @@
+Python is introduced by Guido van Russon in the year 1991,It is very simple & easy to Learn.
+It is an Open Source where user-friendly.
+Python have simple syntax easy to understand and accessible.
+
+#features
+1.Easy to Use
+2.Portable
+3.Object-Oriented
+4.Interpreted
+
+#uses
+We can use python in many sectors 
+1.Data Science
+2.Web development
+3.AIML
+4.Game development
