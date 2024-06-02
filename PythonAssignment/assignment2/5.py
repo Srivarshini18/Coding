d8ca8eeb70ff72b2724481b76571a2a7b5339a4c
Srivarshini Noothi <118071643+Srivarshini18@users.noreply.github.com>
2024-06-02 19:12:01 +0530

    Update 5.py

--- PythonAssignment/assignment2/5.py
+++ PythonAssignment/assignment2/5.py
@@ -1,6 +1,6 @@
 #5. Calculate the area and circumference of a circle, taking the diameter of the circle as an input from the console. 
 
-diameter = int(input())# d is twice radius
+d = int(input())# d is twice radius
 radius = d/2 #radius is half od diameter
 pi = 3.14
 Area = pi*radius*radius
