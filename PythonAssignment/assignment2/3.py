b2163c9477312fa092f4d3d3e873ac4dc0aba49a
Srivarshini Noothi <118071643+Srivarshini18@users.noreply.github.com>
2024-06-02 18:24:13 +0530

    Create 3.py

---
+++ PythonAssignment/assignment2/3.py
@@ -0,0 +1,6 @@
+#Write a program to evaluate the equation, y = x^3 + 7 taking the inputs x from the console.
+#Execute it for various combinations of input x, and note down your observations. 
+
+x = int(input())
+y = x^3 + 7
+print(y)
