61c3ff602f3505bf969219b0e8dd8c4e9c3c5ba5
Srivarshini Noothi <118071643+Srivarshini18@users.noreply.github.com>
2024-07-19 23:02:55 +0530

    Create 3.py

---
+++ assignment3/3.py
@@ -0,0 +1,7 @@
+3. List out the difference between mutable and immutable data types.
+
+In Python, objects are categorized as either mutable or immutable based on whether their values can be changed after creation.
+Lists, dictionaries, sets.
+
+Cannot be changed after creation: Once created, their value is fixed. Any attempt to modify them will create a new object with the updated value.
+Strings, integers, floats, tuples.
