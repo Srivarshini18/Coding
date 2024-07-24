66e329af3f099a3de36e085c09abea36c5d8f279
Srivarshini Noothi <118071643+Srivarshini18@users.noreply.github.com>
2024-07-24 22:29:32 +0530

    Create 2.py

---
+++ PythonAssignment/assignment5/2.py
@@ -0,0 +1,15 @@
+#2. Write a program to find whether the number is positive, negative or zero. Without using inbuilt functions, print the absolute value of the number. 
+
+a = int(input())
+if(a>0):
+  print("Postive number")
+elif(a<0):
+  print("Negative number")
+else:
+  print("zero")
+
+Input 1 : 0
+o/p : zero
+
+Input 2: 3
+o/p : Positive number
